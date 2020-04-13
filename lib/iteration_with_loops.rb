fd4ef15176da4d26d89cfef212f21b45db8ec4ed
Luis Bernal <luis_bernal8010@yahoo.com>
2020-04-13 23:07:54 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,7 +1,17 @@
 require 'pry'
 def join_nested_strings(src)
-  a = []
-  src.each do |x,y|
-    binding.pry
-end
-end
+  outer_results = []
+  row_index = 0
+  while row_index < src.count do
+    element_index = 0
+    while element_index < src[row_index].count do
+
+      if src[row_index][element_index][0] == "P"
+        outer_results << src[row_index][element_index]
+      end
+      element_index += 1
+    end
+    row_index += 1
+  end
+
+  outer_results
